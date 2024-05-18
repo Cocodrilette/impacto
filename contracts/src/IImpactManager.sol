e0352961e03d1642d116e2f19c2c404de9bf91a5
cocodrilette <cocodrilette@gmail.com>
2024-05-18 15:16:47 -0500

    donate function

---
+++ contracts/src/IImpactManager.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: NO LICENSE
+pragma solidity 0.8.20;
+
+contract IImpactManager {
+    event Donated(address indexed donor, uint256 amount);
+}
--- contracts/src/ImpactManager.sol
+++ contracts/src/ImpactManager.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: NO LICENSE
 pragma solidity 0.8.20;
 
-contract ImpactManager {}
+import "./IImpactManager.sol";
+
+contract ImpactManager is IImpactManager {
+    mapping(address => uint256) donations;
+
+    function donate() public payable {
+        if (msg.value == 0) revert("Debes donar algo");
+        donations[msg.sender] += msg.value;
+
+        emit Donated(msg.sender, msg.value);
+    }
+}
