428580db64d6a01511144030c6040b88731b63dc
AnotherDev <joseluismanco37@gmail.com>
2024-05-19 15:23:34 -0400

    automatic increasing

--- front/components/TotalAllocation.tsx
+++ front/components/TotalAllocation.tsx
@@ -1,40 +1,66 @@
-"use client";
+'use client';
 
-import { useReadContract } from "wagmi";
-import React, { useEffect, useState } from "react";
+import { useReadContract } from 'wagmi';
+import React, { useEffect, useState } from 'react';
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
   CardTitle,
-} from "./ui/card";
-import { motion, animate, useMotionValue, useTransform } from "framer-motion";
-import { abi as impactManagerAbi } from "@/constants/abi/impact-manager";
-import { ImpactManagerAddress } from "@/constants";
-import { formatUnits } from "viem";
-import { calculateAllocation } from "@/function/calculateAllocation";
+} from './ui/card';
+import { motion, animate, useMotionValue, useTransform } from 'framer-motion';
+import { abi as impactManagerAbi } from '@/constants/abi/impact-manager';
+import { ImpactManagerAddress } from '@/constants';
+import { formatUnits } from 'viem';
+import { calculateAllocation } from '@/function/calculateAllocation';
 
 function TotalAllocation() {
-  const [initialValues, setInitialValues] = useState(null);
-  const [allocation, setAllocation] = useState("0");
+  const [initialValues, setInitialValues] = useState<null | {
+    starttime: number;
+    lifetime: number;
+    target: number;
+    milestones: {
+      compliance: number;
+      weight: number;
+    }[];
+  }>(null);
+  const [allocation, setAllocation] = useState('0');
   const { data, refetch, error, isFetched } = useReadContract({
     abi: impactManagerAbi,
     address: ImpactManagerAddress,
-    functionName: "getAllocation",
+    functionName: 'getAllocation',
     args: [0, 2],
   });
 
   useEffect(() => {
-    console.debug("data", data);
-  }, [isFetched]);
+    setInitialValues({
+      starttime: 1714589575,
+      lifetime: 1716145243,
+      target: 100000,
+      milestones: [
+        {
+          compliance: 100,
+          weight: 10,
+        },
+        {
+          compliance: 100,
+          weight: 20,
+        },
+        {
+          compliance: 100,
+          weight: 70,
+        },
+      ],
+    });
+  }, []);
 
   useEffect(() => {
     if (!initialValues) return;
     const updateAllocation = () => {
       const t = Math.floor(Date.now() / 1000); // Current time in seconds
       const newAllocation = calculateAllocation(t, initialValues, 1);
-      setAllocation(newAllocation.toFixed(2)); // Adjust as needed
+      setAllocation(newAllocation.toString()); // Adjust as needed
     };
 
     const intervalId = setInterval(updateAllocation, 1000);
@@ -42,18 +68,18 @@ function TotalAllocation() {
   }, [initialValues]);
 
   return (
-    <Card className="allocation w-full">
+    <Card className='allocation w-full'>
       <CardHeader>
         <CardTitle>Total allocation</CardTitle>
       </CardHeader>
       <CardContent>
         <strong>
           $
-          <motion.span className="text-2xl font-bold">{allocation}</motion.span>{" "}
-          USD{" "}
+          <motion.span className='text-2xl font-bold'>{allocation}</motion.span>{' '}
+          USD{' '}
         </strong>
       </CardContent>
-      <CardFooter className="text-sm">
+      <CardFooter className='text-sm'>
         There is your the total amount allocated to you
       </CardFooter>
     </Card>
