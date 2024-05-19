623d17a3637b7ad60c3765efef44c0e6edcd5ae9
Luis_ <73004377+Another-DevX@users.noreply.github.com>
2024-05-18 19:14:05 -0500

    feat: init front

---
+++ front/app/donate/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Donate() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Donate
---
+++ front/app/providers.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import {PrivyProvider} from '@privy-io/react-auth';
+
+export default function Providers({children}: {children: React.ReactNode}) {
+  return (
+    <PrivyProvider
+      appId="your-privy-app-id"
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
